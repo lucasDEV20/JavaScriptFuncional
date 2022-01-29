4c5b33369c8dbe963c0014925058653d0b9a1eda
lucasDEV20 <lucasgoias11@gmail.com>
2022-01-29 15:05:07 -0300

    remanejando projeto

---
+++ JavaScriptFuncional/anatomia.js
@@ -0,0 +1,12 @@
+//function declration
+
+//forma mais simples para criar function
+
+function sayHello(){
+    console.log("Hello!");
+//{ = chave de abertura
+
+//} chave  de fechamento
+// nao e nao retorna nata
+}
+sayHello();
